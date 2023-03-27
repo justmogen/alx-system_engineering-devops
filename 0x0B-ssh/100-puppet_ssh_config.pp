2402419a75ceca4958d55863b0be29b1521fb100
justmogen <justmogen@gmail.com>
2023-03-27 12:39:48 +0300

    using puppet to re-configure a client server configuration file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 file_line { 'Turn off passwd auth':
-  path  => '/etc/ssh/sshd_config',
+  path  => '/etc/ssh/ssh_config',
   line  => 'PasswordAuthentication no',
   match => '^#?PasswordAuthentication',
 }
