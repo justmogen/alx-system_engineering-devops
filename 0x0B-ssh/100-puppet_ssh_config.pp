bdf3628220e0951877fda65b88d2e664ac6038b1
justmogen <justmogen@gmail.com>
2023-03-27 12:36:32 +0300

    using puppet to re-configure a client server configuration file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 file_line { 'Turn off passwd auth':
-  path  => '/home/justmogen/alx-system_engineering-devops/0x0B-ssh/2-ssh_config',
+  path  => '/etc/ssh/sshd_config',
   line  => 'PasswordAuthentication no',
   match => '^#?PasswordAuthentication',
 }
 
 file_line { 'Declare identity file':
-  path  => '/home/justmogen/alx-system_engineering-devops/0x0B-ssh/2-ssh_config',
+  path  => '/etc/ssh/ssh_config',
   line  => 'IdentityFile ~/.ssh/school',
   match => '^#?IdentityFile',
 }
