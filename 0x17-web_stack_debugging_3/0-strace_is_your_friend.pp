9196d4afbc2e4cb86cca307add55a5d1b7fc91b3
justmogen <justmogen@gmail.com>
2023-06-14 09:17:02 +0300

    puppet automation file for error 500 in Apache

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,17 +1,17 @@
 # Fix the typo in /var/www/html/wp-settings
 
 file { '/var/www/html/wp-settings.php':
-ensure => file,
-source => '/var/www/html/wp-settings.php',
+ensure  => file,
+source  => '/var/www/html/wp-settings.php',
 content => file('/var/www/html/wp-settings.php').content.gsub('class-wp-locale.phpp', 'class-wp-locale.php'),
-owner => 'www-data',
-group => 'www-data',
-mode => '0644',
-notify => Service['apache2'],
+owner   => 'www-data',
+group   => 'www-data',
+mode    => '0644',
+notify  => Service['apache2'],
 }
 
 service { 'apache2':
-ensure => running,
-enable => true,
+ensure  => running,
+enable  => true,
 require => File['/var/www/html/wp-settings.php'],
 }
