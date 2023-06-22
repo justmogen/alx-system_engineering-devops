df8f09e009cbbd215673817843b1414318d29c1a
justmogen <justmogen@gmail.com>
2023-06-22 08:35:27 +0300

    correction: write the rows iteratively instead of using a list coomprehesion. For each item,we write a new row to the CSV file using the 'writer.writerow()' method

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -14,6 +14,6 @@ if __name__ == "__main__":
 
     with open("{}.csv".format(user_id), "w", newline="") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        [writer.writerow(
-            [user_id, username, t.get("completed"), t.get("title")]
-        ) for t in todos]
+        for todo in todos:
+           writer.writerow([user_id, username, todo.get("completed"), todo.get("title")])
+
