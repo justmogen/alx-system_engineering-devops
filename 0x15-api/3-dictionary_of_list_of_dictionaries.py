4e1326cae715d95db6b8838a5926cf2e5baf2525
justmogen <justmogen@gmail.com>
2023-06-22 09:30:47 +0300

    export all information of to-do list of specified employee

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -7,13 +7,13 @@ import requests
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     users = requests.get(url + "users").json()
-    
+
     with open("todo_all_employees.json", "w") as jsonfile:
         json.dump({
             user.get("id"): [{
                 "tasks": to.get("title"),
                 "completed": to.get("completed"),
-                "username": user.get ("username")
+                "username": user.get("username")
             } for to in requests.get(url + "todos",
-                            params={"userId": user.get("id")}).json()]
+                                     params={"userId": user.get("id")}).json()]
             for user in users}, jsonfile)
