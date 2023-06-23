0bafe6fc0fef2ec74c341bc4126436687fa04851
justmogen <justmogen@gmail.com>
2023-06-23 08:10:56 +0300

    retrieve total number of subscribers froma subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,10 +2,11 @@
 """this function will query number of subscribers on specified subreddit"""
 import requests
 
+
 def number_of_subscribers(subreddit):
     """Returns total subscribers on the given subreddit"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json".format(subreddit)
-    headers = {"User-Agent": "Custom User Agent"} 
+    headers = {"User-Agent": "Custom User Agent"}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
